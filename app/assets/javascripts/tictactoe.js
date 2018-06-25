68cc8b444505cbce97cb36fa33ac7f96cdccb73a
Sean Clifford <sclifford6262@gmail.com>
2018-06-25 23:26:43 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -48,9 +48,9 @@ $(function(){
       let tdArray = $("td") //creates array of td elements
       const state = r.data.attributes["state"] //grabs state of selected game from rails server
       for(let i = 0; i < state.length; i++){
-        if(state[i] === "X"){
+        if(state[i] == "X"){
           window.turn++
-        } else if(state[i] === "O"){
+        } else if(state[i] == "O"){
           debugger;
           window.turn++
         }
