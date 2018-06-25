8c4946297e4c0426183e3b73e57079ad077c6159
Sean Clifford <sclifford6262@gmail.com>
2018-06-25 23:39:45 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,10 +3,10 @@
 function player(){
   return (window.turn % 2 == 0) ? "X" : "O"
 }
+window.turn = 0//define a turn
 
 $(function(){
   //when adding the ability to start a previous game, switch thisGame!!
-  window.turn = 0//define a turn
   let thisGame //set a game ID to determine if this game has already been saved
   //saves or updates current game
   $("#save").on("click", function(e){
