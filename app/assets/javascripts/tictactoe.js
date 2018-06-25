4efd95dbbdc963021ab16a5e06dd36bcbb9c2926
Sean Clifford <sclifford6262@gmail.com>
2018-06-25 18:55:40 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,46 @@
-// Code your JavaScript / jQuery solution here
+$(function(){
+  //when adding the ability to start a previous game, switch thisGame!!
+  let thisGame //set a game ID to determine if this game has already been saved
+  //saves or updates current game
+  $("#save").on("click", function(e){
+    e.preventDefault()
+    let state = [] //set state array to pass as JSON to rails server
+    $("td").each(function(){
+      state.push(this.textContent)
+    })
+    //if this current game has been saved already there will be an ID stored in thisGame
+    //if not a new game will be created and saved, and its id will be set as thisGame
+    if (!!thisGame) {
+      $.ajax({
+        url: "/games/" + thisGame,
+        type: "PUT",
+        data: {state},
+        success: function(r){alert("Game " + r.data["id"] + " updated!")}
+      })
+    } else {
+      $.post('/games', {state}, function(r){
+        thisGame = parseInt(r.data["id"])
+        alert("New game " + thisGame + " saved!")
+      })
+    }
+  })
+  //shows previous games
+  $("#previous").on("click", function(e){
+    e.preventDefault()
+    $.get('/games', function(r){
+      let gameList = "<ul>"
+      r.data.forEach(function(game){
+        gameList += `<li class="oldGame" data-id="${game["id"]}">` + game["id"] + "</li>"
+      })
+      gameList += "</ul>"
+      $("#games").html(gameList)
+    })
+  })
+  //reloads a previous games
+  $("li.oldGame").on("click", function(e){
+    e.preventDefault()
+    debugger;
+    //$.get("/games" + )
+  })
+
+})
