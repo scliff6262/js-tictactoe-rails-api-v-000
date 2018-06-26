dc7655c06ba0b91e4b81bbdd76e0f8752eed51fe
Sean Clifford <sclifford6262@gmail.com>
2018-06-26 00:34:49 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -80,3 +80,14 @@ function player(){
 function updateState(element){
   element.textContent = player()
 }
+
+const winningNumbers = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
