9c160d3997e39eb2c7a962b57ee495cc1925677d
Sean Clifford <sclifford6262@gmail.com>
2018-06-26 00:47:37 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,8 +1,8 @@
 // Code your JavaScript / jQuery solution here
+let state = 
 $(function(){
   //when adding the ability to start a previous game, switch thisGame!!
   window.turn = 0//define a turn
-  let state;
   let thisGame //set a game ID to determine if this game has already been saved
   //saves or updates current game
   $("#save").on("click", function(e){
