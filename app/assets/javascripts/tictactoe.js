d491559f5f410ac4985d99645a87a1f26b080b8a
Sean Clifford <sclifford6262@gmail.com>
2018-06-26 01:17:52 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -98,7 +98,7 @@ function updateState(element){
 function checkWinner(){
   winningNumbers.forEach(function(nums){
     if (gameState[nums[0]] === gameState[nums[1]] && gameState[nums[1]] === gameState[nums[2]]){
-      setMessage(currentBoard[nums[1]])
+      setMessage(gameState[nums[1]])
       return true
     } 
   })
